c9a618826ee1f0f4e957ed429f8615d0f80b9bcd
Sean Fairchild <sfairchild@nexient.com>
2017-02-22 17:47:56 -0500

    Solution to Raindrops

---
+++ ruby/raindrops/raindrops.rb
@@ -0,0 +1,16 @@
+class Raindrops
+  SOUNDS = {
+    Pling: 3,
+    Plang: 5,
+    Plong: 7
+  }
+
+  def self.convert number
+    sounds = SOUNDS.select { |sound, size| number % size == 0 }
+    sounds.any? ? sounds.keys.join : number.to_s
+  end
+end
+
+module BookKeeping
+  VERSION = 3
+end
