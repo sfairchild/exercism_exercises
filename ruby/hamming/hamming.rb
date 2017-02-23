1ce729720ac23eb403e6b08e3c208ec7da26a113
Sean Fairchild <sfairchild@nexient.com>
2017-02-23 11:20:00 -0500

    Made some changes to Hamming after some suggestions

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -2,22 +2,23 @@ module BookKeeping
   VERSION = 3
 end
 
-class Hamming
-  def self.compute first, second
-    raise ArgumentError unless first.length == second.length
-    first_strand, second_strand = Strand.new(first), Strand.new(second)
-    first_strand.-(second_strand).length
-  end
-end
+module Hamming
+  def self.compute strand_one, strand_two
+    raise ArgumentError.new('stands are not equal length') unless strand_one.length == strand_two.length
 
-class Strand
-  attr_accessor :sequence
-
-  def initialize sequence
-    @sequence = sequence.split ''
+    first_strand, second_strand = Strand.new(strand_one), Strand.new(strand_two)
+    (first_strand - second_strand).length
   end
 
-  def - other_strand2
-    sequence.zip(other_strand2.sequence).reject{|pair| pair.first == pair.last}
+  class Strand
+    attr_accessor :sequence
+
+    def initialize sequence
+      @sequence = sequence.chars
+    end
+
+    def - other_strand
+      sequence.zip(other_strand.sequence).reject { |n1, n2| n1 == n2 }
+    end
   end
 end
